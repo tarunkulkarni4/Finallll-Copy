bb84bdd050abf31a15a00b1022e72f73a23c27b5
Tarun chidambar Kulakarni <tarunkulakarni2@gmail.com>
2024-10-05 22:08:20 +0530

    Update orderController.js

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -11,7 +11,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder=async(req,res)=>{
 
-    const frontend_url="http://localhost:5174";
+    const frontend_url="https://food-del-backend-kjhg.onrender.com";
     try {
         const newOrder=new orderModel({
             userId:req.body.userId,
